a1b56ed02d9aa8aed04f1f0204b347dd19b72ede
daniel <danielchinaemelum@gmail.com>
2022-06-05 11:35:44 -0700

    8 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 /**
- *  main - prints exactly and that piece of art is useful 
- *  Dora Korpar, 2015-10-19,
- *  followed by a new line, to the standard error.
- *  Return: Always 0 (Success)
+ * main - prints exactly and that piece of art is useful
+ *  Dora Korpar, 2015-10-19
+ *  followed by a new line, to the standard error
+ *  Return: 1 if succeed
  */
 
 int main(void)
 
 {
-
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
